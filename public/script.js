a8f503deb41bba45fd0a7b6be4cc8b680adfed67
Fernando Herrera <fernando@ciedigital.com>
2017-07-07 16:42:34 -0700

    completed app

--- public/script.js
+++ public/script.js
@@ -1,33 +1,96 @@
+var PRICE = 9.99,
+    LOAD_NUM = 10;
+
 new Vue({
     el: "#app",
     data: {
         total: 0,
-        items: [
-            { title: "Item 1", id: 1 },
-            { title: "Item 2", id: 2 },
-            { title: "Item 3", id: 3 },
-            { title: "Item 4", id: 4 }
-        ],
-        cart: []
+        items: [],
+        cart: [],
+        results: [],
+        newSearch: 'mustang',
+        lastSearch: '',
+        loading: false,
+        price: PRICE
+    },
+    computed: {
+        noMoreItems: function() {
+            return this.items.length === this.results.length && this.results.length > 0
+        }
     },
     methods: {
+        appendItems: function () {
+            if (this.items.length < this.results.length) {
+                var append = this.results.slice(this.items.length, this.items.length + LOAD_NUM);
+                this.items = this.items.concat(append);
+            }
+        },
+        onSubmit: function() {
+            if (!this.newSearch.length) {
+                return false;
+            }
+            this.items = [];
+            this.loading = true;
+            this.$http
+                .get('/search/' + this.newSearch)
+                .then(function(res){
+                    this.lastSearch = this.newSearch;
+                    this.results = res.data;
+                    this.appendItems();
+                    this.loading = false;
+                })
+            ;
+        },
         addItem: function(index) {
-            this.total += 9.99;
+            this.total += PRICE;
             var item = this.items[index];
             var found = false;
             for (var i = 0; i < this.cart.length; i++) {
                 if (this.cart[i].id === item.id) {
                     found = true;
                     this.cart[i].qty++;
+                    break;
                 }
             }
             if (!found) {
                 this.cart.push({
                     id: item.id,
                     title: item.title,
-                    qty: 1
+                    qty: 1,
+                    price: PRICE
                 });
             }
+        },
+        inc: function (item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+        dec: function (item) {
+            item.qty--;
+            this.total -= PRICE;
+            if (item.qty <= 0) {
+                for (var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            }
         }
+    },
+    filters: {
+        currency: function (price) {
+            return '$' + price.toFixed(2);
+        }
+    },
+    mounted: function() {
+        this.onSubmit();
+
+        var that = this;
+        var elem = document.getElementById('product-list-bottom');
+        var watcher = scrollMonitor.create(elem);
+        watcher.enterViewport(function() {
+            that.appendItems();
+        });
     }
 });
